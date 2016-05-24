2a87464bb78df372d4d9ef1dc6e181abe735e689
Jimmy Patel <jpatel@scu.edu>
2016-05-24 13:38:10 -0700

    Comments Added

--- app/main.rb
+++ app/main.rb
@@ -5,26 +5,32 @@ require 'erb'
 require 'sass'
 require './student'
 
+#The following configure is used for session managament
 configure do
   enable :sessions
   set :username, 'frank'
   set :password, 'sinatra'
 end
 
+#In develop mode we will be using the local student.db file
 configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/student.db")
 end
 
+#Tells the app what DB to use in production mode
 configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
+#route to css
 get('/styles.css'){ scss :styles }
 
+#route to login
 get '/login' do
   erb :login
 end
 
+#route to get the params back from the login page
 post '/login' do
   if params[:username] == settings.username &&
     params[:password] == settings.password
@@ -35,24 +41,29 @@ post '/login' do
   end
 end
 
+#route to logout page
 get '/logout' do
   session.clear
   redirect to ("/")
 end
 
+#route to home page
 get '/' do
   erb :home
 end
 
+#route to about page
 get '/about' do
   @title = "All About This Website"
   erb :about
 end
 
+#route to contact page
 get '/contact' do
   erb :contact
 end
 
+#What to diaplay when page is not found.
 not_found do
   erb :not_found
 end
--- app/student.rb
+++ app/student.rb
@@ -6,6 +6,7 @@ require 'dm-migrations'
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/student.db")
 
+#This is the student class to create an object from the information read from student.db
 class Student
   include DataMapper::Resource
   property :id, Serial
@@ -18,49 +19,60 @@ class Student
     super Date.strptime(date, '%m/%d/%Y')
   end
 end
+
+#Allows us to modify the table without completely deleting it.
 DataMapper.auto_upgrade!
 
+#Session handling
 configure do
   enable :sessions
   set :username, 'frank'
   set :password, 'sinatra'
 end
 
+#Check for validity of DB
 DataMapper.finalize
 
+#route to students page and chreates an object of all the students
 get '/students' do
   @students = Student.all
   erb :students
 end
 
+#route to add a new student and checks if you are logged in
 get '/students/new' do
   halt(401, "Not Authorized") unless session[:admin]
   @student = Student.new
   erb :new_student
 end
 
+#route to display the page for individual student
 get '/students/:id' do
   @student = Student.get(params[:id])
   erb :show_student
 end
 
+#route to page to edit a student and checks if you are logged in
 get '/students/:id/edit' do
   halt(401, "Not Authorized") unless session[:admin]
   @student = Student.get(params[:id])
   erb :edit_student
 end
 
+#Information passed back from creating a new student comes here
 post '/students' do  
   student = Student.create(params[:student])
   redirect to("/students/#{student.id}")
 end
 
+#route to update student information
 put '/students/:id' do
   student = Student.get(params[:id])
   student.update(params[:student])
   redirect to("/students/#{student.id}")
 end
 
+#route to delete a student and check to see if you are logged in
 delete '/students/:id' do
   halt(401, "Not Authorized") unless session[:admin]
   Student.get(params[:id]).destroy
